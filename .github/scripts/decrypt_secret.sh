e917074215c61b350b2b9612d28245e0b6ff3b61
Listen 1 <githublisten1@gmail.com>
2020-10-31 22:00:20 +0800

    fix gpg file path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,4 +3,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $GITHUB_WORKSPACE/android/app/my-release-key.keystore ./github/scripts/my-release-key.keystore.gpg
+--output $GITHUB_WORKSPACE/android/app/my-release-key.keystore $GITHUB_WORKSPACE/.github/scripts/my-release-key.keystore.gpg
