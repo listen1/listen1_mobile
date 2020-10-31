71a44a067c38e4851678b6e4ac4c03db885b0088
Listen 1 <githublisten1@gmail.com>
2020-10-31 21:53:37 +0800

    add encrypt keystone file and workflow

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $GITHUB_WORKSPACE/android/app/my-release-key.keystore ./github/scripts/my-release-key.keystore.gpg
